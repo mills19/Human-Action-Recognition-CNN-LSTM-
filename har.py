07e021da3ded275a1df0e13f0eb03f6a121be186
Mills :) <mukherjeesoumili123@gmail.com>
2025-03-26 13:11:38 +0530

    Update har.py

--- har.py
+++ har.py
@@ -503,36 +503,10 @@ plt.xlabel('Epoch')
 plt.legend(['Train', 'Validation'], loc='upper left')
 plt.show()
 
-import matplotlib.pyplot as plt
-# Plot training & validation accuracy values
-plt.figure(figsize=(10, 5))
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Validation'], loc='upper left')
-plt.show()
 
-# Plot training & validation loss values
-plt.figure(figsize=(10, 5))
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Validation'], loc='upper left')
-plt.show()
 
-import matplotlib.pyplot as plt
-# Plot training & validation accuracy values
-plt.figure(figsize=(10, 5))
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Validation'], loc='upper left')
-plt.show()
+
+
+
 
 
